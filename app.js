f5032e435bf2fa9c8ec153c45d93592dab3e09c5
Iulia Bivolaru <ibivolaru@norauto.com>
2018-12-08 20:33:20 +0100

    Display game log and style it

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ new Vue({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame() {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack() {
-            this.monsterHealth -= this.calculateDamage(3, 10);
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
             this.monsterAttacks();
         },
         specialAttack() {
-            this.monsterHealth -= this.calculateDamage(10, 20);
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits monster hard for ' + damage
+            });
             if (this.checkWin()) {
                 return;
             }
@@ -29,10 +41,14 @@ new Vue({
             if(this.playerHealth <= 90) {
                 this.playerHealth += 10;
             } else this.playerHealth = 100;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10'
+            });
             this.monsterAttacks();
         },
         giveUp() {
-
+            this.gameIsRunning = false;
         },
         calculateDamage(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -56,8 +72,13 @@ new Vue({
             return false;
         },
         monsterAttacks() {
-            this.playerHealth -= this.calculateDamage(5, 12);
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits player for ' + damage
+            })
         }
     }
 })
